b9194de520d750887d0580565b93ef7d386acc61
Dennis Kao <dshnkao@gmail.com>
2017-02-04 02:09:09 +1100

    Initial Commit

---
+++ SpaceId/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpaceId
+//
+//  Created by Dennis Kao on 4/2/17.
+//  Copyright © 2017 Dennis Kao. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
